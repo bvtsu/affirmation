d77e7ecf0b7ead6ea576bbef747a883f7e85448f
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-10-16 22:06:52 -0700

    Added more affirmations

--- src/Affirmations/AffirmationText.py
+++ src/Affirmations/AffirmationText.py
@@ -2,5 +2,13 @@
 affirmations = [
     "You're Great!",
     "Way to Go!",
-    "Woo hoo!"
+    "Woo hoo!",
+    "You got out of bed this morning!",
+    "Keep going!",
+    "What nimble fingers you have!",
+    "Awesome!",
+    "Did you ever know that you're my hero?",
+    "Lookin' good.",
+    "You're on your way to glory",
+    "You can do it!"
 ]
